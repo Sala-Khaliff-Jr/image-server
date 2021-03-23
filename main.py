fb46b9a55ac9d20ec3c798963c4820f023956a9c
salakhaliff <salakhaliffjr@outlook.com>
2021-03-23 10:00:23 +0530

    file upload completed

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
-from flask import Flask
-import os 
-
-CURENT_FOLDER = os.getcwd()
-UPLOAD_FOLDER = CURENT_FOLDER+'images'
-ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+"""Program to start a web server for image-server
+"""
+from flask import Flask, render_template, request, url_for, redirect
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload/success')
+def upload_success():
+    return render_template('upload_success.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['uploaded_file']
+    print(uploaded_file.__dict__)
+    if uploaded_file.filename != '':
+        uploaded_file.save(
+            os.path.join('images/', secure_filename(uploaded_file.filename))
+        )
+    return redirect(url_for('upload_success'))
+
+
+# uploaded_file
+#     {'name': 'uploaded_file',
+#      'stream': <tempfile.SpooledTemporaryFile object at 0x7fd292af8e80>, 
+#     filename': 'ai_5_logo_alone.png', 
+#         'headers': Headers(
+#             [('Content-Disposition', 
+#             'form-data; name="uploaded_file";  filename="ai_5_logo_alone.png"'), 
+#             ('Content-Type', 'image/png')]
+#         )
+#     }
